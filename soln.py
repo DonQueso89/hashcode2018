86e92593bd8f60f16c43ed18e797b492ea9a41c5
Kees van Ekeren <keesvanekeren@Keess-MacBook-Pro.local>
2018-03-01 20:23:08 +0100

    Naive stupid implementation

--- soln.py
+++ soln.py
@@ -1,15 +1,19 @@
+import random
 import sys
+import pendulum
 from collections import namedtuple
+
 
 # latest is the step after the vehicle has arrived
 # NOT the step at which the vehicle arrives
 class Ride(object):
-    def __init__(self, start, end, t_start, t_end, waiting):
+    def __init__(self, start, end, t_start, t_end, waiting, ind):
         self.start = start
         self.end = end
         self.t_start = t_start
         self.t_end = t_end
         self.waiting = waiting
+        self.ind = ind
 
 
 class Vehicle(object):
@@ -21,19 +25,51 @@ class Vehicle(object):
         self.pos = pos
         self.occupied = False
 
+    def __str__(self):
+        s = str(len(self.rides))
+        for r in self.rides:
+            s += ' %d' % r.ind
+        s += '\n'
+        return s
 
-def get_rides(fname):
-    inp = open(fname, 'r')
-    R, C, F, N, B, T = map(int, next(inp).split(' '))
+    def assign(self, ride):
+        self.rides.append(ride)
+
+
+def get_rides(fp):
     rides = []
-    for line in inp:
+    ind = 0
+    for line in fp:
         a, b, x, y, s, f = map(int, line.split(' '))
-        rides.append(Ride((a, b), (x, y), s, f, waiting=True))
+        rides.append(Ride((a, b), (x, y), s, f, True, ind))
+        ind += 1
     return rides
 
 
 def main(fname):
-    rides = get_rides(fname)
+    fp = open(fname, 'r')
+    R, C, F, N, B, T = map(int, next(fp).split(' '))
+    # init rides
+    rides = get_rides(fp)
+    # init vehicles
+    vehicles = []
+    for i in range(F):
+        vehicles.append(Vehicle((0, 0)))
+
+    for ride in rides:
+        random.choice(vehicles).assign(ride)
+
+    output(vehicles, fname)
+
+
+def output(vehicles, fname):
+    with open('%soutput%s' % (fname, pendulum.now().isoformat()), 'a') as outp:
+        for v in vehicles:
+            outp.write(str(v))
+
+
+def score(vehicles):
+   pass
 
 
 def distance(coord1, coord2):
