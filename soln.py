940bea18e39dccb868447923f7ada660f30f06c9
Kees van Ekeren <keesvanekeren@Keess-MacBook-Pro.local>
2018-03-01 22:57:44 +0100

    Added broken  sol

--- soln.py
+++ soln.py
@@ -1,19 +1,13 @@
 import random
 import sys
 import pendulum
+import numpy as np
 from collections import namedtuple
 
 
 # latest is the step after the vehicle has arrived
 # NOT the step at which the vehicle arrives
-class Ride(object):
-    def __init__(self, start, end, t_start, t_end, waiting, ind):
-        self.start = start
-        self.end = end
-        self.t_start = t_start
-        self.t_end = t_end
-        self.waiting = waiting
-        self.ind = ind
+Ride = namedtuple('Ride', ['start', 'end', 't_start', 't_end', 'ind'])
 
 
 class Vehicle(object):
@@ -23,7 +17,7 @@ class Vehicle(object):
         """
         self.rides = rides or []
         self.pos = pos
-        self.occupied = False
+        self.current_ride = None
 
     def __str__(self):
         s = str(len(self.rides))
@@ -33,7 +27,15 @@ class Vehicle(object):
         return s
 
     def assign(self, ride):
+        self.current_ride = ride
         self.rides.append(ride)
+
+    def unassign(self):
+        self.current_ride = None
+
+    @property
+    def r(self):
+        return self.current_ride
 
 
 def get_rides(fp):
@@ -41,7 +43,7 @@ def get_rides(fp):
     ind = 0
     for line in fp:
         a, b, x, y, s, f = map(int, line.split(' '))
-        rides.append(Ride((a, b), (x, y), s, f, True, ind))
+        rides.append(Ride(np.array([a, b]), np.array([x, y]), s, f, ind))
         ind += 1
     return rides
 
@@ -53,23 +55,53 @@ def main(fname):
     rides = get_rides(fp)
     # init vehicles
     vehicles = []
+
     for i in range(F):
-        vehicles.append(Vehicle((0, 0)))
+        vehicle = Vehicle(np.array([0, 0]))
+        rides = optimal_path(vehicle, rides, T)
+        vehicles.append(vehicle)
 
-    for ride in rides:
-        random.choice(vehicles).assign(ride)
-
+    rides = sorted(rides, key=lambda x: x.t_start, reverse=True)
     output(vehicles, fname)
+
+
+def closest_ride(vehicle, rides):
+    closest = random.choice(rides)
+    for r in rides:
+        if distance(r.start, vehicle.pos) < distance(closest.start, vehicle.pos):
+            closest = r
+   
+    return closest
+
+
+def optimal_path(vehicle, rides, T):
+    t = 0
+    attempt = 0
+    while t < T and len(rides) > 0:
+        closest = closest_ride(vehicle, rides)
+        if attempt == 1:
+            closest = random.choice(rides)
+            attempt = 0
+        dist_to_start = distance(vehicle.pos, closest.start)
+        dist_ride = distance(closest.start, closest.end)
+        if (T - t) < (dist_to_start + dist_ride):
+            break
+        # Can the vehicle still do the ride?
+        if dist_to_start + t < closest.t_end - dist_ride:
+            vehicle.assign(closest)
+            t += dist_to_start
+            t += dist_ride
+            vehicle.pos = closest.end
+            rides = [r for r in rides if r.ind != closest.ind]
+            continue
+        attempt += 1
+    return rides
 
 
 def output(vehicles, fname):
     with open('%soutput%s' % (fname, pendulum.now().isoformat()), 'a') as outp:
         for v in vehicles:
             outp.write(str(v))
-
-
-def score(vehicles):
-   pass
 
 
 def distance(coord1, coord2):
